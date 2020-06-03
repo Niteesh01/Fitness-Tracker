58014dbb4660a6e8958d05c4fcdd2f8fd89943ea
Akurati <AKU1700@corp.stateauto.com>
2020-06-03 18:56:25 -0400

    Making Authentication functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import {StopTrainingComponent} from './training/current-training/stop-training.component';
+import {A} from '@angular/cdk/keycodes';
+import {AuthService} from './auth/auth.service';
 
 
 
@@ -44,7 +46,7 @@ import {StopTrainingComponent} from './training/current-training/stop-training.c
     AppRoutingModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
   entryComponents: [StopTrainingComponent]
 })
